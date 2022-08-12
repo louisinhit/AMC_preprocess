5f6862ca1d6c11a0a0347b055b85141985d5c997
louisinhit <30955307+louisinhit@users.noreply.github.com>
2022-08-12 11:56:46 +1000

    Create train_test.sh

---
+++ train_test.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+classifier="LDA"   # choose from 'LRG' 'LDA' 'SGD' 
+cuda=true   # otherwise "False"
+
+python3 gen_subset.py
+python3 linear_trans.py
+python3 second_trans.py  --classifier $classifier  --cuda $cuda
+python3 high_trans.py  --classifier $classifier  --cuda $cuda
